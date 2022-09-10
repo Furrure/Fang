8f76300fef3f026628b835349d58d5379e8c75b3
Jacob Scrapchansky <71794171+Furrure@users.noreply.github.com>
2022-09-10 10:15:27 -0400

    Initial Setup

---
+++ Server/Unit.py
@@ -0,0 +1,6 @@
+from NodeConnector import *
+
+class Unit:
+	def __init__(self, node_interface_port, ):
+		pass
+
---
+++ install_node.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+#Currently only designed to work with systemd-enabled systems! Please use sudo for the installer.
